3810ea611159a4e940cc3bf49d4801341131d4c1
Jalaj Kumar Gaur <raajk0338@gmail.com>
2021-06-28 09:51:17 +0530

    make it better

---
+++ routes/users.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const plm = require('passport-local-mongoose');
+
+// mongoose.connect('mongodb://localhost/gfaadbook');
+mongoose.connect('mongodb+srv://jkthecoder:jkTheCoder@cluster0.va5hj.mongodb.net/chatapp?retryWrites=true&w=majority', {
+  useNewUrlParser:true,
+  useFindAndModify:false,
+  useUnifiedTopology:true
+}).then( () => console.log('Database Configured!'))
+.catch( err => console.log(err));
+
+var userSchema = mongoose.Schema({
+  username: String,
+  password: String,
+  name: String,
+  profilePic: {
+    type: String,
+    default: '/images/user-avaatar.png'
+  },
+  posts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'posts'
+  }],
+  email: String,
+  about: String,
+  city: String,
+  resetToken: String,
+  resetTime: String
+})
+
+userSchema.plugin(plm);
+
+module.exports = mongoose.model('user', userSchema);
